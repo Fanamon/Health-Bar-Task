a974f4fcbbf7e80391e37ca436af3a0a660fcd08
Fanamon <shatalov1998@yahoo.com>
2023-06-20 06:31:27 +0500

    Optimized Health Bar Script Code and combined small action elements

--- Assets/Scripts/HealthBarTask/BarDisplayOutput.cs
+++ Assets/Scripts/HealthBarTask/BarDisplayOutput.cs
@@ -7,10 +7,13 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Animator))]
 public class BarDisplayOutput : MonoBehaviour
 {
-    [SerializeField] private TMP_Text _healthText;
+    [SerializeField] private TMP_Text _barText;
+    [SerializeField] private Image _barImage;
     [SerializeField] private Slider _healthBar;
+    [SerializeField] private Color _lowValueColor;
 
     private Animator _animator;
+    private Color _maxValueColor;
 
     private bool _isCurrentBarValueNull;
 
@@ -19,11 +22,20 @@ public class BarDisplayOutput : MonoBehaviour
     private void Awake()
     {
         _animator = GetComponent<Animator>();
+
+        LoadFromColor();
     }
 
-    private void Update()
+    [ContextMenu("Load From Color")]
+    private void LoadFromColor()
     {
-        _isCurrentBarValueNull = _healthBar.value == 0;
+        _maxValueColor = _barImage.color;
+    }
+
+    public void SetPosition(float position)
+    {
+        _barText.color = Color.Lerp(_lowValueColor, _maxValueColor, position);
+        _barImage.color = Color.Lerp(_lowValueColor, _maxValueColor, position);
     }
 
     public void SetAnimatorTrigger(string triggerName)
@@ -33,11 +45,13 @@ public class BarDisplayOutput : MonoBehaviour
 
     public void WriteBarText(float currentValue, float maxValue)
     {
-        _healthText.text = $"{currentValue} / {maxValue}";
+        _barText.text = $"{currentValue} / {maxValue}";
     }
 
     public void ChangeBarValue(float currentValuePercentage, float timeDelta)
     {
         _healthBar.value = Mathf.MoveTowards(_healthBar.value, currentValuePercentage, timeDelta);
+
+        _isCurrentBarValueNull = _healthBar.value == 0;
     }
 }
