c58571063e33f49f6f3339ef531d4442f681ed69
Nikita Shatalov <shatalov1998@yahoo.com>
2024-07-15 15:39:21 +0500

    Minor

--- Assets/Scripts/HealthBarTask/PeasantAnimatorTrigger.cs
+++
@@ -1,6 +0,0 @@
-public static class PeasantAnimatorTrigger
-{
-    public static readonly string Damaged = "Damaged";
-    public static readonly string Healed = "Healed";
-    public static readonly string Killed = "Killed";
-}
